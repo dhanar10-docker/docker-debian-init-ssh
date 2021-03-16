15256b3507868850bd78e6ba66f193b35a6c173e
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2021-03-17 03:19:20 +0700

    deploy.sh: initial commit

---
+++ deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# https://developers.redhat.com/blog/2016/09/13/running-systemd-in-a-non-privileged-container/
+
+set -e
+set -o pipefail
+
+if [ ! -d /sys/fs/cgroup/systemd ]; then
+        sudo mkdir -p /sys/fs/cgroup/systemd
+        sudo mount -t cgroup cgroup -o none,name=systemd /sys/fs/cgroup/systemd
+fi
+
+docker build -t debian-init-ssh .
+docker run -d --restart unless-stopped \
+    -v /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    --tmpfs /run \
+    --tmpfs /run/lock \
+    --tmpfs /tmp \
+    --cap-add SYS_ADMIN \
+    "$@" \
+    debian-init-ssh
+    
